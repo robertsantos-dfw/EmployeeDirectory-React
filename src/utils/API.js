e4fa8a08e88c01c3374f69b46794d4fe922bccc6
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-08 14:50:35 -0600

    added RandomUser API

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
-  }
+  // Gets all users
+  getUsers: function () {
+    return axios.get("https://randomuser.me/api/?results=250&nat=us");
+  },
 };
