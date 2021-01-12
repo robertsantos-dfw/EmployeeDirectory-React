123ab23411a1710c83b99024716ddcb5c3694d88
Robert Santos <robertsantos.dfw@gmail.com>
2021-01-11 21:39:18 -0600

    default header

---
+++ employeedirectory-react/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Filter or search by name to get a specific results.</p>
+    </div>
+  );
+}
+
+export default Header;
